e931d1a292a53ae8d72e99654f33e47159a4851b
Y <thejfex@proton.me>
2025-03-04 10:23:24 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+  DDRB |= (1 << DDB5); // Set pin 13 (PB5) as output
+
+  while (1) {
+    PORTB ^= (1 << PORTB5); // Toggle pin 13
+    _delay_ms(1000); // 1 second delay
+  }
+
+  return 0;
+}
